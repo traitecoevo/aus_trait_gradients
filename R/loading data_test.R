eb9e71bc631531ccb496ed423423974c7b66494a
itowers1 <39288928+itowers1@users.noreply.github.com>
2023-02-21 14:58:08 +1100

    clean up folders

--- R/loading data_test.R
+++ R/loading data_test.R
@@ -1,7 +1,6 @@
 library("tidyverse")
 
-load_climate_data <- function(trait_data){
-  browser()
+load_climate_data <- function(trait_data, simple = FALSE){
   
 same <- TRUE
 if(file.exists("data/climate_data.RDS")){
@@ -17,7 +16,7 @@ trait_data %>%
     group_by(longitude, latitude,ID) %>%
     nest() %>%
     ungroup() %>%
-    dplyr::select(-data) %>% sample_n(1)-> trait_data_coords
+    dplyr::select(-data)-> trait_data_coords
   
   
 folders <- c("WC","envirem","wc2.1_30s_bio","VPD_Chelsa")  
@@ -38,6 +37,40 @@ extracted[[4]] <- terra::resample(extracted[[4]],extracted[[1]][[1]], method = "
 
 
 extracted_stacked <- c(extracted[[1]], extracted[[2]], extracted[[3]], extracted[[4]])
+
+if(simple == TRUE){
+occurrence_data_species_family_vect <- terra::vect(trait_data_coords, 
+                                                   geom = c("longitude","latitude"), 
+                                                   crs = terra::crs(extracted_stacked))
+
+terra::extract(x = extracted_stacked,  y = occurrence_data_species_family_vect,method = "simple", cells = TRUE, bind = TRUE, ID = FALSE) -> extracted_simple
+
+extracted_simple <- terra::values(extracted_simple)
+extracted_simple
+
+extracted_simple %>%
+  rename(wc2.1_30s_prec.wq = wc2.1_30s_bio_16,
+         wc2.1_30s_prec.dq = wc2.1_30s_bio_17,
+         wc2.1_30s_prec.cv = wc2.1_30s_bio_15,
+         wc2.1_30s_temp.cv = wc2.1_30s_bio_4) %>%
+  rename_with(~str_remove(string = ., pattern = "wc2.1_30s_"), .cols = starts_with("wc")) %>%
+  rename_with(~str_remove(string = ., pattern = "current_30arcsec_"), .cols = starts_with("current_30arcsec_")) %>% 
+  rename_with(~str_remove(string = ., pattern = "CHELSA_"), .cols = starts_with("CHELSA_")) %>% 
+  rename_with(~str_remove(string = ., pattern = "_1981-2010_V.2.1"), .cols = ends_with("_1981-2010_V.2.1")) %>% 
+  pivot_longer(-c(ID, cell)) %>%
+  separate(name, into = c("env_name", "month"), sep="_(?=[^_]+$)") %>%
+  group_by(ID, cell, env_name) %>%
+  nest() -> data
+
+data %>%
+  pivot_wider(names_from = env_name, values_from = data) %>%
+  ungroup() %>%
+  mutate(prec = map_dbl(prec, ~.x %>% unnest() %>% summarise(value = sum(value)) %>% pull(value))) %>%
+  mutate(across(!c(ID, prec, cell), ~map_dbl(., ~.x %>% unnest() %>% summarise(value = mean(value)) %>% pull(value))))-> climate_data
+
+
+saveRDS(climate_data, "data/climate_data_simple.RDS")
+}
 
 extract_values <- function(start, end){
   extracted_stacked[[start:end]] %>%
